a984aeace83162e4fd723ebe405dd9c6a00cab67
Dong-Yeon Park <58678384+DyeonPark@users.noreply.github.com>
2023-02-17 16:14:20 +0900

    Create 합성수 찾기.py

---
+++ Programmers/합성수 찾기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1, n+1):
+        for j in range(2, i):
+            if i % j == 0:
+                answer += 1
+                break
+                
+    return answer

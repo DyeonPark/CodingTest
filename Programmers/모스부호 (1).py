625def450e613f452c13472afb031602aac97418
Dong-Yeon Park <58678384+DyeonPark@users.noreply.github.com>
2023-02-17 16:14:37 +0900

    Create 모스부호 (1).py

---
+++ Programmers/모스부호 (1).py
@@ -0,0 +1,14 @@
+def solution(letter):
+    
+    morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    
+    letter_list = list(letter.split())
+    answer_list = [morse[i] for i in letter_list]
+    
+    return ''.join(answer_list)

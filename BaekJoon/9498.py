62366a8eb4e6439c5645c9f40b81cedc2a4f13a2
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-06-06 12:09:43 +0900

    시험 성적

---
+++ BaekJoon/9498.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+if N >= 90 and N <= 100:
+  print("A")
+elif N >= 80 and N < 90:
+  print("B")
+elif N >= 70 and N < 80:
+  print("C")
+elif N >= 60 and N < 70:
+  print("D")
+else:
+  print("F")

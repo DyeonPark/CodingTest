69dbc9d8b7aea756d7d94efa3840d86f23a62006
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-09-07 18:34:06 +0900

    정다면체: 시간복잡도 및 강사풀이 코드 추가

--- 파이썬-알고리즘-문제풀이-코딩테스트/섹션2/정다면체.py
+++ 파이썬-알고리즘-문제풀이-코딩테스트/섹션2/정다면체.py
@@ -1,4 +1,5 @@
 # solved : 21m
+# 전체 코드 시간복잡도 : O(N^2 * M)
 import sys
 input = sys.stdin.readline
 
@@ -18,7 +19,6 @@ for i in sum_list: # O(N*M)
 maximum = max(cnt) # O(N)
 
 res = set()
-
 # O(N*M)
 for idx, count in enumerate(cnt):
     if count == maximum:
@@ -27,3 +27,25 @@ for idx, count in enumerate(cnt):
 # O(NlogN)
 for i in sorted(list(res)):
     print(i, end=' ')
+
+
+# 강사 풀이 : 가능한 합을 카운팅하는 방식으로 했는데, 이게 훨씬 시간복잡도도 공간복잡도도 절약할 수 있는 방법같다
+# 강사 풀이를 보고 스스로 다시 작성한 코드 (전체 코드 시간복잡도 : O(N*M))
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+sum_list = [0] * (N + M + 1)
+
+# O(N*M)
+for i in range(1, N + 1):
+  for j in range(1, M + 1):
+    sum_list[i + j] += 1
+
+# O(N*M)
+maximum = max(sum_list)
+
+# O(N*M)
+for idx, value in enumerate(sum_list):
+  if value == maximum:
+    print(idx, end=" ")

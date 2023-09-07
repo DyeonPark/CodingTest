df21b2ae54322496254eeeb11287da10a43da9bc
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-09-07 21:45:22 +0900

    뒤집은 소수: 코드 수정 및 주석 추가

--- 파이썬-알고리즘-문제풀이-코딩테스트/섹션2/뒤집은 소수.py
+++ 파이썬-알고리즘-문제풀이-코딩테스트/섹션2/뒤집은 소수.py
@@ -8,16 +8,17 @@ prime_list = [1] * (MAX_SIZE + 1)
 prime_list[0] = 0
 prime_list[1] = 0
 
-for i in range(2, MAX_SIZE):
-    j = 2
-    while j * i <= MAX_SIZE:
-        prime_list[j * i] = 0
-        j += 1
+for i in range(2, MAX_SIZE + 1):
+    if prime_list[i] == 1: # 반복문을 도는 횟수를 줄이기 위해서 if문을 추가
+        j = 2
+        while j * i <= MAX_SIZE:
+            prime_list[j * i] = 0
+            j += 1
 
-def isPrime(x):
+def isPrime(x): # 강의 풀이는 이 부분을 for문을 돌며 해당 숫자가 소수인지 판별하는 방식으로 풀이하였음
     return prime_list[x]
 
-def reverse(x):
+def reverse(x): # 강의 풀이는 이 부분을 '% 10', '* 10', '// 10' 연산을 사용하여 풀이하였음
     return int(str(x)[::-1])
 
 N = int(input())

1ce1f7abc1d82cc5ea5cc011528607dcc44306a3
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-09-07 11:04:11 +0900

    정다면체

---
+++ 파이썬-알고리즘-문제풀이-코딩테스트/섹션2/정다면체.py
@@ -0,0 +1,29 @@
+# solved : 30m
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+sum_list = []
+cnt = []
+
+# O(N*M)
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        sum_list.append(i + j)
+
+# O(N^2 * M)
+for i in sum_list: # O(N*M)
+    cnt.append(sum_list.count(i)) # O(N)
+    
+maximum = max(cnt) # O(N)
+
+res = set()
+
+# O(N*M)
+for idx, count in enumerate(cnt):
+    if count == maximum:
+        res.add(sum_list[idx])
+
+# O(NlogN)
+for i in sorted(list(res)):
+    print(i, end=' ')

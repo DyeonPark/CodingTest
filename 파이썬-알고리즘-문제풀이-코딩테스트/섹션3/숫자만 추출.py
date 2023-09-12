998e730faa5afb5febf0e38d64f181ee2e6e3578
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-09-12 09:45:05 +0900

    숫자만 추출: 답안풀이 비교 주석 추가

--- 파이썬-알고리즘-문제풀이-코딩테스트/섹션3/숫자만 추출.py
+++ 파이썬-알고리즘-문제풀이-코딩테스트/섹션3/숫자만 추출.py
@@ -6,10 +6,15 @@ def input():
 
 N = input()
 num = ""
+# num = 0  # 답안 풀이
 
 for n in N:
+  # 답안에서는 n.isdecimal()을 사용
+  # isdigit() : 숫자처럼 생긴 특수문자도 True를 내뱉음
+  # isdecimal() : int형으로 변환 가능한 것에만 True를 내뱉음 (즉, 특수문자로 된 숫자는 False가 나옴)
   if n.isdigit():
     num += n
+    # num = num * 10 + int(n)  # 답안 풀이
     
 num = int(num)
 cnt = 0

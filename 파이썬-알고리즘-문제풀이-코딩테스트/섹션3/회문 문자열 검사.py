d50ee55a498ba16877d954c768bfd8f7d16519b0
Dong-Yeon Park (박동연_T5080) <58678384+DyeonPark@users.noreply.github.com>
2023-09-12 09:41:55 +0900

    회문 문자열 검사: 답안 비교 및 주석 추가

--- 파이썬-알고리즘-문제풀이-코딩테스트/섹션3/회문 문자열 검사.py
+++ 파이썬-알고리즘-문제풀이-코딩테스트/섹션3/회문 문자열 검사.py
@@ -1,13 +1,14 @@
-# solved: 4m
+# solved : 4m
 import sys
 
 def input():
   return sys.stdin.readline().rstrip()
 
 N = int(input())
-for n in range(N):
+for n in range(1, N + 1):
   word = input()
-  for i, j in zip(word.upper(), word[::-1].upper()):
+  # if word.uppper() == word[::-1].upper(): # 이와 같은 형태로 비교할 수도 있음 (시간복잡도는 동일)
+  for i, j in zip(word.upper(), word[::-1].upper()): # 이 for문은 절반만 돌도록 수정해도 좋음
     if i != j:
       print(f"#{n} NO")
       break
